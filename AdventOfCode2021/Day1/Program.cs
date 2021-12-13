ef026d4ca21c840939c8d42a04f9936072109cfe
Jacob Postema <jacob.postema@deluxe.com>
2021-12-13 11:55:53 -0500

    Added gitignore and solved Day 1 part1

--- AdventOfCode2021/Day1/Program.cs
+++ AdventOfCode2021/Day1/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = System.IO.File.ReadAllLines(@"/Users/t463971/code/aoc2021/AdventOfCode2021/Day1/input.txt");
+int[] report = Array.ConvertAll(lines, int.Parse);
+ 
+Console.WriteLine("########## Day 1 2020 ##########");
+Console.WriteLine($"Part one solution: {SolvePartOne(report)}");
+// Console.WriteLine($"Part two solution: {SolvePartTwo(report)}");
+Console.WriteLine("################################");
+
+static int SolvePartOne(int[] report)
+{
+    int increaseNumber = 0;
+    for (int i = 1; i < report.Length; i++)
+    {
+        if(report[i] > report[i - 1])
+        {
+            increaseNumber++;
+        }
+
+    }
+    return increaseNumber;
+}
 
