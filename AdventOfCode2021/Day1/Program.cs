a22456e3c80321f9f3c416d199e8e4b92c4ff03d
Jacob Postema <jacob.postema@deluxe.com>
2021-12-15 11:22:10 -0500

    Sloved Day 2 Part 1

--- AdventOfCode2021/Day1/Program.cs
+++ AdventOfCode2021/Day1/Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] lines = System.IO.File.ReadAllLines(@"/Users/t463971/code/aoc2021/AdventOfCode2021/Day1/input.txt");
 int[] report = Array.ConvertAll(lines, int.Parse);
  
-Console.WriteLine("########## Day 1 2020 ##########");
+Console.WriteLine("########## Day 1 2021 ##########");
 Console.WriteLine($"Part one solution: {SolvePartOne(report)}");
 Console.WriteLine($"Part two solution: {SolvePartTwo(report)}");
 Console.WriteLine("################################");
---
+++ AdventOfCode2021/Day2/Program.cs
@@ -0,0 +1,46 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"/Users/t463971/code/aoc2021/AdventOfCode2021/Day2/input.txt");
+var commands = new List<KeyValuePair<string, int>>();
+
+foreach (var line in lines)
+{
+    string[] command = line.Split(' ');
+    commands.Add(new KeyValuePair<string, int>(command[0], int.Parse(command[1])));
+}
+
+Console.WriteLine("########## Day 2 2021 ##########");
+Console.WriteLine($"Part one solution: {SolvePartOne(commands)}");
+Console.WriteLine($"Part two solution: {SolvePartTwo(commands)}");
+Console.WriteLine("################################");
+
+static int SolvePartOne(List<KeyValuePair<string, int>> commands)
+{
+    var horizontal_position = 0;
+    var depth = 0;
+
+    foreach (var item in commands)
+    {
+        switch (item.Key)
+        {
+            case "forward":
+                horizontal_position += item.Value;
+                break;
+            
+            case "down":
+                depth += item.Value;
+                break;
+
+            case "up":
+                depth -= item.Value;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return depth * horizontal_position;
+}
+
+static int SolvePartTwo(List<KeyValuePair<string, int>> commands)
+{   
+    return 1;
+}
